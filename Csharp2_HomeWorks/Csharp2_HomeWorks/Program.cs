5d7c3d30b91502aca268656ab8e83a2db5ca2d56
Sergey <pomazkin.sergey@gmail.com>
2018-12-03 23:27:18 +0500

    создан новый проект

---
+++ Csharp2_HomeWorks/Csharp2_HomeWorks/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp2_HomeWorks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
